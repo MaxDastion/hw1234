7bf9d3e859b946253775d531b4fcfb9cb7fd833f
User <User@DESKTOP-3V2P9PB>
2024-04-06 13:48:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Class.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class BaseClass
+{
+    public virtual string VirtualProperty { get; set; }
+
+    public BaseClass(params object[] parameters)
+    {
+        foreach (object param in parameters)
+        {
+            Console.WriteLine("Parameter: " + param.ToString());
+        }
+    }
+
+    public virtual void VirtualMethod()
+    {
+        Console.WriteLine("BaseClass: Virtual Method");
+    }
+}
+
+public class DerivedClass1 : BaseClass
+{
+    public int AdditionalParameter { get; }
+
+    public override string VirtualProperty
+    {
+        get => base.VirtualProperty;
+        set => base.VirtualProperty = value;
+    }
+
+    public DerivedClass1(int additionalParameter, params object[] parameters)
+        : base(parameters)
+    {
+        AdditionalParameter = additionalParameter;
+    }
+
+    public override void VirtualMethod()
+    {
+        Console.WriteLine("DerivedClass1: Virtual Method");
+    }
+}
+
+public class DerivedClass2 : DerivedClass1
+{
+    public double AnotherAdditionalParameter { get; }
+
+    public override string VirtualProperty
+    {
+        get => base.VirtualProperty;
+        set => base.VirtualProperty = value;
+    }
+
+    public DerivedClass2(double anotherAdditionalParameter, int additionalParameter, params object[] parameters)
+        : base(additionalParameter, parameters)
+    {
+        AnotherAdditionalParameter = anotherAdditionalParameter;
+    }
+
+    public override void VirtualMethod()
+    {
+        Console.WriteLine("DerivedClass2: Virtual Method");
+    }
+}
+
---
+++ ConsoleApp4/MyMain.cs
@@ -0,0 +1,14 @@
+﻿
+class MyMain
+{
+    static void Main(string[] args)
+    {
+        BaseClass baseObj = new BaseClass(1, "test", true);
+        DerivedClass1 derived1Obj = new DerivedClass1(10, "example", false);
+        DerivedClass2 derived2Obj = new DerivedClass2(3.14, 20, "sample", true);
+
+        baseObj.VirtualMethod();
+        derived1Obj.VirtualMethod();
+        derived2Obj.VirtualMethod();
+    }
+}
